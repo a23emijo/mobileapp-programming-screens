cbdba9c154c60618c6da2ade400b92ac7bf4262d
a23emijo <a23emijo@student.his.se>
2024-04-15 16:39:23 +0200

    Added a button on the second activity to get back to main with constraints

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -1,15 +1,27 @@
 package com.example.screens;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private Button buttonToMainActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-    }
+        buttonToMainActivity = findViewById(R.id.goToMainActivity);
 
+        buttonToMainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SecondActivity.this, MainActivity.class));
+            }
+        });
+    }
 }
