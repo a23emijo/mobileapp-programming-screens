f9ac2f9bf01bc7fc5cfc5583bff97be86d7b0c93
a23emijo <a23emijo@student.his.se>
2024-04-16 09:11:10 +0200

    Added the main activity as a parent to the second activity and added some info texts

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -9,18 +9,26 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Variables
     private Button buttonToSecondActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonToSecondActivity = findViewById(R.id.goToSecondActivity);
+        buttonToSecondActivity = findViewById(R.id.goToSecondActivity); // Finds the to second activity button
 
+
+
+        // Sets a OnClickListener for the to second activity button
         buttonToSecondActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                // Sets the intents
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("vehicle", "Car"); // Sets the vehicle type
+                intent.putExtra("color", "Red"); // Sets the color of the vehicle to red
+                startActivity(intent); // Starts the activity with the intents
             }
         });
     }
--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -4,19 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    // Variables
     private Button buttonToMainActivity;
+    private TextView vehicleText;
+    private TextView colorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        buttonToMainActivity = findViewById(R.id.goToMainActivity);
+        buttonToMainActivity = findViewById(R.id.goToMainActivity); // Finds the go to main button
+        vehicleText = findViewById(R.id.vehicleText); // Finds the vehicle-TextView
+        colorText = findViewById(R.id.colorText); // Finds the color-TextView
 
+        // Gets the intents
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String vehicle = extras.getString("vehicle");
+            String color = extras.getString("color");
+            vehicleText.setText(vehicle); // Sets the text to the vehicle inputted in MainActivity
+            colorText.setText(color); // Sets the text to the color inputted in MainActivity
+        }
+
+        // Sets a OnClickListener for the back to main activity button
         buttonToMainActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
